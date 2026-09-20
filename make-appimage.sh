8b511bf075ad71bbcd19dcdd543677e942402ed3
ArqamQazi <arqamqazi549@gmail.com>
2026-09-20 10:35:08 +0530

    patch wrapper script

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,17 +12,28 @@ export ICON=/usr/share/icons/hicolor/256x256/apps/org.x.Warpinator.png
 export DEPLOY_PYTHON=1
 
 export PATH_MAPPING='
-	/usr/lib/warpinator:${SHARUN_DIR}/lib/warpinator
 	/usr/share/warpinator:${SHARUN_DIR}/share/warpinator
 	/usr/share/locale:${SHARUN_DIR}/share/locale
 '
 
 # Deploy dependencies
-quick-sharun /usr/bin/warpinator* /usr/lib/warpinator /usr/share/warpinator /usr/lib/libgtk-3.so*
+quick-sharun \
+  /usr/bin/warpinator* \
+  /usr/lib/warpinator \
+  /usr/share/warpinator \
+  /usr/lib/libgtk-3.so* \
+  /usr/lib/libxapp.so*
 
 # Deploy additional .desktop files
 mkdir -p ./AppDir/share/applications
 cp -f /etc/xdg/autostart/warpinator-autostart.desktop ./AppDir/share/applications/
+
+# Patch wrapper scripts portably to use AppDir
+for bin in ./AppDir/bin/warpinator*; do
+  [ -f "$bin" ] || continue
+  sed 's|/usr|"$APPDIR"|g' "$bin" >"$bin.tmp" && mv -f "$bin.tmp" "$bin"
+  chmod 755 "$bin"
+done
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
